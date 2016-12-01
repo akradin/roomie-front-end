d0f48c1311473aacb8db3838040e39d1a211509e
akradin <alex.kradin@gmail.com>
2016-12-01 10:53:41 -0500

    Delete directory that is not needed
    Remove extranous functions that are now on a separate branch

--- assets/scripts/chore-api/chore-ui.js
+++ assets/scripts/chore-api/chore-ui.js
@@ -3,10 +3,10 @@
 
 
 const createSuccess = (data) => {
-    $('.chore-id').html("ID: " + " " + data.chore.id + "<br>");
-    $('.chore-name').html("NAME: " + " " + data.chore.name + "<br>");
-    $('.chore-due').html("DUE DATE: " + " " + data.chore.due_date + "<br>" );
-    $('.chore-difficulty').html("DIFFICULTY: " + " " + data.chore.difficulty + "<br>");
+    $('.chore-id').html("ID: " + " " + data.id + "<br>");
+    $('.chore-name').html("NAME: " + " " + data.name + "<br>");
+    $('.chore-due').html("DUE DATE: " + " " + data.due_date + "<br>" );
+    $('.chore-difficulty').html("DIFFICULTY: " + " " + data.difficulty + "<br>");
 
 };
 
@@ -18,8 +18,8 @@ const updateSuccess = (data) =>{
 
 const showSuccess = function(data){
   let chores = "";
-  for (let i = 0; i < data.chores.length; i++) {
-     chores += "NAME: " + " " + data.chores[i].name + "<br>" + "ID: " + " "  + data.chores[i].id + "<br>" + "DUE DATE: " + " " + data.chores[i].due_date + "<br>" + "DIFFICULTY: " + " " + data.chores[i].difficulty + "<br>";
+  for (let i = 0; i < data.length; i++) {
+     chores += "NAME: " + " " + data[i].name + "<br>" + "ID: " + " "  + data[i].id + "<br>" + "DUE DATE: " + " " + data[i].due_date + "<br>" + "DIFFICULTY: " + " " + data[i].difficulty + "<br>";
   }
   $('.show-chores').html(chores);
   $('.chore-id').hide();
