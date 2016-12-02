d27eb040382fa2e202d69eb5d1187bc86bd608d0
akradin <alex.kradin@gmail.com>
2016-12-02 11:41:48 -0500

    Grunt build

--- dependencies.js
+++ dependencies.js
@@ -100,7 +100,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(2);
-	module.exports = __webpack_require__(30);
+	module.exports = __webpack_require__(51);
 
 
 /***/ },
@@ -10332,7 +10332,7 @@
 
 /***/ },
 
-/***/ 30:
+/***/ 51:
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(jQuery) {/*!
